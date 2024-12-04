993231cbba252f611ac11048fa4cf3b4c713ea44
rj7041103 <jeyjeyelcracv@gmail.com>
2024-12-04 16:24:56 -0400

    Fix problem with migrations

--- Products/models.py
+++ Products/models.py
@@ -8,7 +8,7 @@ class Products(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.CharField(max_length=300)
+    image = models.CharField(max_length=300, null=True)
     created_at = models.DateTimeField(auto_now_add=True)  
     class Meta:
         db_table = "products"
@@ -23,7 +23,7 @@ class Inventory(models.Model):
     presentation = models.CharField(max_length=100)
     cuant = models.IntegerField()
     cost = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.CharField(max_length=300)
+    image = models.CharField(max_length=300, null=True)
     created_at = models.DateTimeField(auto_now_add=True)  
     class Meta:
         db_table = "inventory"
--- server/settings.py
+++ server/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
     #default='postgresql://postgres:postgres@localhost:5432/mysite',
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://coffee_shop_bakend_user:8tyOqBGghWv2KgKrrU4UT3tD8qDTYQif@dpg-csfv6m56l47c739kmrfg-a/coffee_shop_bakend',
+        default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
     
